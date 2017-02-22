c143a5331f8957ca3e8d0696acb771c74bc307fa
Tomoaki Kawada <tcpp.jp@gmail.com>
2017-02-22 12:31:58 +0900

    Add sdl-stopwatch example (wip)

---
+++ test/sdl-stopwatch/tMainWindowCore.c
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2017 Tomoaki Kawada
+ *
+ * This file is part of tcwtecs.
+ *
+ * tcwtecs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * tcwtecs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with tcwtecs.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* #[<PREAMBLE>]#
+ * Don't edit the comments between #[<...>]# and #[</...>]#
+ * These comment are used by tecsmerege when merging.
+ *
+ * call port function #_TCPF_#
+ * call port: cDC signature: sTWDrawingContext context:task
+ *   void           cDC_fillRect( TWColor color, const TWRect* rect );
+ *   void           cDC_drawBitmap( const char* data, TWPixelFormat format, const TWSize* bitmapSize, uint32_t numBytes, const TWRect* inRect, const TWPoint* outLoc, TWColor monoColor );
+ * call port: cStopwatch signature: sStopwatchControl context:task
+ *   void           cStopwatch_start( );
+ *   void           cStopwatch_stop( );
+ *   void           cStopwatch_freeze( );
+ *   void           cStopwatch_unfreeze( );
+ *   void           cStopwatch_reset( );
+ *   bool           cStopwatch_isRunning( );
+ *   bool           cStopwatch_isFreezed( );
+ *
+ * #[</PREAMBLE>]# */
+
+#include <stdio.h>
+#include "tMainWindowCore_tecsgen.h"
+
+#ifndef E_OK
+#define	E_OK	0		/* success */
+#define	E_ID	(-18)	/* illegal ID */
+#endif
+
+/* entry port function #_TEPF_# */
+/* #[<ENTRY_PORT>]# ePaintEvent
+ * entry port: ePaintEvent
+ * signature:  sTWPaintEvent
+ * context:    task
+ * #[</ENTRY_PORT>]# */
+
+/* #[<ENTRY_FUNC>]# ePaintEvent_paint
+ * name:         ePaintEvent_paint
+ * global_name:  tMainWindowCore_ePaintEvent_paint
+ * oneway:       false
+ * #[</ENTRY_FUNC>]# */
+void
+ePaintEvent_paint(CELLIDX idx)
+{
+	CELLCB	*p_cellcb = GET_CELLCB(idx);
+
+	int w = 960, h = 640;
+	TWRect r;
+
+	TWSetRect(&r, 0, 0, w, h);
+	cDC_fillRect(TWMakeColor(192, 192, 192), &r);
+}
+
+/* #[<ENTRY_PORT>]# eStartStopButtonActivated
+ * entry port: eStartStopButtonActivated
+ * signature:  sAction
+ * context:    task
+ * #[</ENTRY_PORT>]# */
+
+/* #[<ENTRY_FUNC>]# eStartStopButtonActivated_activated
+ * name:         eStartStopButtonActivated_activated
+ * global_name:  tMainWindowCore_eStartStopButtonActivated_activated
+ * oneway:       false
+ * #[</ENTRY_FUNC>]# */
+void
+eStartStopButtonActivated_activated(CELLIDX idx)
+{
+    CELLCB  *p_cellcb = GET_CELLCB(idx);
+
+}
+
+/* #[<ENTRY_PORT>]# eResetLapButtonActivated
+ * entry port: eResetLapButtonActivated
+ * signature:  sAction
+ * context:    task
+ * #[</ENTRY_PORT>]# */
+
+/* #[<ENTRY_FUNC>]# eResetLapButtonActivated_activated
+ * name:         eResetLapButtonActivated_activated
+ * global_name:  tMainWindowCore_eResetLapButtonActivated_activated
+ * oneway:       false
+ * #[</ENTRY_FUNC>]# */
+void
+eResetLapButtonActivated_activated(CELLIDX idx)
+{
+    CELLCB  *p_cellcb = GET_CELLCB(idx);
+
+}
+
+/* #[<POSTAMBLE>]#
+ *   Put non-entry functions below.
+ * #[</POSTAMBLE>]#*/
---
+++ test/sdl-stopwatch/tTestApp.c
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2017 Tomoaki Kawada
+ *
+ * This file is part of tcwtecs.
+ *
+ * tcwtecs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * tcwtecs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with tcwtecs.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* #[<PREAMBLE>]#
+ * Don't edit the comments between #[<...>]# and #[</...>]#
+ * These comment are used by tecsmerege when merging.
+ *
+ * call port function #_TCPF_#
+ * call port: cSDL signature: sTWSDLGraphicsDeviceControl context:task
+ *   void           cSDL_initialize( );
+ * call port: cSDLDispatcher signature: sTWSDLDispatcherControl context:task
+ *   void           cSDLDispatcher_initialize( );
+ *   void           cSDLDispatcher_enterMainLoop( );
+ *   void           cSDLDispatcher_quit( );
+ * call port: cDesktop signature: sTWDesktopControl context:task
+ *   void           cDesktop_paint( );
+ *   void           cDesktop_repaintAll( );
+ *
+ * #[</PREAMBLE>]# */
+
+/* Put prototype declaration and/or variale definition here #_PAC_# */
+#include "tTestApp_tecsgen.h"
+
+#ifndef E_OK
+#define	E_OK	0		/* success */
+#define	E_ID	(-18)	/* illegal ID */
+#endif
+
+/* entry port function #_TEPF_# */
+/* #[<POSTAMBLE>]#
+ *   Put non-entry functions below.
+ * #[</POSTAMBLE>]#*/
+
+int main(int argc, char **argv) {
+    SDL_Init(SDL_INIT_TIMER | SDL_INIT_EVENTS | SDL_INIT_VIDEO);
+    cSDLDispatcher_initialize();
+	cSDL_initialize();
+	cDesktop_repaintAll();
+	cSDLDispatcher_enterMainLoop();
+}
