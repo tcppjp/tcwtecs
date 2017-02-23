279878d11f1e2cc12052f1e96254a9aa83a92f6b
Tomoaki Kawada <tcpp.jp@gmail.com>
2017-02-24 02:05:54 +0900

    Update tTWRGB565BitmapGraphicsRenderer

--- src/tTWRGB565BitmapGraphicsRenderer.c
+++ src/tTWRGB565BitmapGraphicsRenderer.c
@@ -22,7 +22,11 @@
  * These comment are used by tecsmerege when merging.
  *
  * attr access macro #_CAAM_#
+ * numScanlineClipperNodes uint16_t         ATTR_numScanlineClipperNodes
  * scissor          TWRect           VAR_scissor
+ * scanlineClipper  TWScanlineClipperState  VAR_scanlineClipper
+ * scanlineClipperLineScanner TWScanlineClipperLineScanState  VAR_scanlineClipperLineScanner
+ * scanlineClipperNodes TWScanlineClipperNode*  VAR_scanlineClipperNodes
  *
  * call port function #_TCPF_#
  * call port: cRenderTargetBitmapSource signature: sTWRenderTargetBitmapSource context:task
@@ -47,14 +51,17 @@
 #endif
 
 static inline uint16_t TWColorToRGB565(TWColor color) {
-	uint16_t ret = (color.red + 4) >> 3;
-	ret |= ((uint16_t)((color.green + 2) >> 2)) << 5;
-	ret |= ((uint16_t)((color.blue + 4) >> 3)) << 11;
+	uint16_t ret = color.red >> 3;
+	ret |= ((uint16_t)(color.green >> 2)) << 5;
+	ret |= ((uint16_t)(color.blue >> 3)) << 11;
 	return ret;
 }
 
 static inline uint16_t RGBX32ToRGB565(uint32_t rgbx32) {
-
+	uint16_t ret = (rgbx32 >> 3) & 0x1f;
+	ret |= ((uint16_t)(rgbx32 >> 5)) & 0x7e0;
+	ret |= ((uint16_t)(rgbx32 >> 8)) & 0xf800;
+	return ret;
 }
 
 /* entry port function #_TEPF_# */
@@ -79,6 +86,37 @@ eGraphicsDeviceOutput_getScreenSize(CELLIDX idx, TWSize* outSize)
 	outSize->h = h;
 }
 
+/* #[<ENTRY_FUNC>]# eGraphicsDeviceOutput_setClippingRect
+ * name:         eGraphicsDeviceOutput_setClippingRect
+ * global_name:  tTWRGB565BitmapGraphicsRenderer_eGraphicsDeviceOutput_setClippingRect
+ * oneway:       true
+ * #[</ENTRY_FUNC>]# */
+void
+eGraphicsDeviceOutput_setClippingRect(CELLIDX idx, const TWRect* rect)
+{
+	CELLCB	*p_cellcb = GET_CELLCB(idx);
+
+	TWScanlineClipperInitialize(&VAR_scanlineClipper, VAR_scanlineClipperNodes, ATTR_numScanlineClipperNodes);
+	TWScanlineClipperSetClippingRect(&VAR_scanlineClipper, rect);
+
+	TWScanlineClipperInitializeLineScanner(&VAR_scanlineClipperLineScanner);
+}
+
+/* #[<ENTRY_FUNC>]# eGraphicsDeviceOutput_subtractClippingRect
+ * name:         eGraphicsDeviceOutput_subtractClippingRect
+ * global_name:  tTWRGB565BitmapGraphicsRenderer_eGraphicsDeviceOutput_subtractClippingRect
+ * oneway:       true
+ * #[</ENTRY_FUNC>]# */
+void
+eGraphicsDeviceOutput_subtractClippingRect(CELLIDX idx, const TWRect* rect)
+{
+	CELLCB	*p_cellcb = GET_CELLCB(idx);
+
+	TWScanlineClipperSubtractClippingRect(&VAR_scanlineClipper, rect);
+
+	TWScanlineClipperInitializeLineScanner(&VAR_scanlineClipperLineScanner);
+}
+
 /* #[<ENTRY_FUNC>]# eGraphicsDeviceOutput_setScissorRect
  * name:         eGraphicsDeviceOutput_setScissorRect
  * global_name:  tTWRGB565BitmapGraphicsRenderer_eGraphicsDeviceOutput_setScissorRect
@@ -88,10 +126,8 @@ void
 eGraphicsDeviceOutput_setScissorRect(CELLIDX idx, const TWRect* rect)
 {
 	CELLCB	*p_cellcb = GET_CELLCB(idx);
-	uint16_t w, h;
-	cRenderTargetBitmapSource_get(NULL, NULL, &w, &h);
-	TWRect bounds = {0, 0, (int16_t)w, (int16_t)h};
-	TWRectIntersect(rect, &bounds, &VAR_scissor);
+
+	VAR_scissor = *rect;
 }
 
 /* #[<ENTRY_FUNC>]# eGraphicsDeviceOutput_fillRect
@@ -117,17 +153,34 @@ eGraphicsDeviceOutput_fillRect(CELLIDX idx, TWColor color, const TWRect* rect)
 		return;
 	}
 
+	uint_fast16_t pixel = TWColorToRGB565(color);
+
 	pixels += x1 + y1 * w;
 
-	int rw = x2 - x1, rh = y2 - y1;
-	uint32_t pixel = TWColorToRGB565(color);
-
-	for (int y = 0; y < rh; ++y) {
-		for (int x = 0; x < rw; ++x) {
-			*(pixels++) = pixel;
-		}
-		pixels += w - rw;
+	TWScanlineClipperSpanScanState spanScanner;
+	if (!TWScanlineClipperMoveToLine(&VAR_scanlineClipper, &VAR_scanlineClipperLineScanner, y1, y2 - y1)) {
+		return;
 	}
+
+	do {
+		pixels += w * VAR_scanlineClipperLineScanner.skip;
+
+		if (!TWScanlineClipperStartLine(&VAR_scanlineClipper, &spanScanner, x1, x2 - x1,
+		  &VAR_scanlineClipperLineScanner)) {
+			goto Continue;
+		}
+
+		uint16_t *pixels2 = pixels;
+		do {
+			pixels2 += spanScanner.skip;
+			for (int i = 0; i < spanScanner.width; ++i) {
+				*(pixels2++) = pixel;
+			}
+		} while (TWScanlineClipperLineAdvance(&spanScanner));
+
+	Continue:
+		pixels += w;
+	} while (TWScanlineClipperMoveToNextLine(&VAR_scanlineClipper, &VAR_scanlineClipperLineScanner));
 }
 
 /* #[<ENTRY_FUNC>]# eGraphicsDeviceOutput_drawBitmap
@@ -136,7 +189,7 @@ eGraphicsDeviceOutput_fillRect(CELLIDX idx, TWColor color, const TWRect* rect)
  * oneway:       true
  * #[</ENTRY_FUNC>]# */
 void
-eGraphicsDeviceOutput_drawBitmap(CELLIDX idx, const char* data, TWPixelFormat format, const TWSize* bitmapSize, uint32_t numBytes, const TWRect* inRect, const TWPoint* outLoc, TWColor monoColor)
+eGraphicsDeviceOutput_drawBitmap(CELLIDX idx, const char* data, uint32_t numBytes, const TWBitmapInfo* info, uint16_t infoSize, const TWRect* inRect, const TWPoint* outLoc)
 {
 	CELLCB	*p_cellcb = GET_CELLCB(idx);
 
@@ -144,20 +197,26 @@ eGraphicsDeviceOutput_drawBitmap(CELLIDX idx, const char* data, TWPixelFormat fo
 	uint16_t tgt_w, tgt_h;
 	cRenderTargetBitmapSource_get((void **)&pixels, NULL, &tgt_w, &tgt_h);
 
-	int src_w = bitmapSize->w, src_h = bitmapSize->h;
+	assert(info);
+	assert(data);
+	assert(infoSize >= sizeof(info->header));
 
-	if (src_w <= 0 || src_h <= 0) {
-		assert(0); // TODO: error report
-		return;
+	TWPixelFormat format = info->header.format;
+
+	switch (format) {
+		case TWPixelFormat1bppMonotone:
+			assert(infoSize >= sizeof(info->mono));
+			break;
+		default:
+			break;
 	}
+
+	int src_w = info->header.width, src_h = info->header.height;
 
 	// size check
 	size_t stride = TWComputeBitmapStride(format, src_w);
 	size_t real_size = stride * src_h; // TODO: overflow check
-	if (numBytes < real_size) {
-		assert(0); // TODO: error report
-		return;
-	}
+	assert(numBytes >= real_size);
 
 	// perform clipping
 	int in_x1 = inRect->x, in_y1 = inRect->y;
@@ -198,56 +257,116 @@ eGraphicsDeviceOutput_drawBitmap(CELLIDX idx, const char* data, TWPixelFormat fo
 	data += in_y1 * stride;
 	pixels += out_x1 + out_y1 * tgt_w;
 
+	TWScanlineClipperSpanScanState spanScanner;
+	if (!TWScanlineClipperMoveToLine(&VAR_scanlineClipper, &VAR_scanlineClipperLineScanner, out_y1, out_y2 - out_y1)) {
+		return;
+	}
+
 	switch (format) {
 		case TWPixelFormat1bppMonotone: {
-			uint32_t pixel = TWColorToRGB565(monoColor);
+			uint_fast16_t pixel1 = TWColorToRGB565(info->mono.palette[0]);
+			uint_fast16_t pixel2 = TWColorToRGB565(info->mono.palette[1]);
 			data += in_x1 >> 3;
 
-			for (int y = out_y1; y < out_y2; ++y) {
+			do {
+				pixels += tgt_w * VAR_scanlineClipperLineScanner.skip;
+				data += stride * VAR_scanlineClipperLineScanner.skip;
+
+				if (!TWScanlineClipperStartLine(&VAR_scanlineClipper, &spanScanner, out_x1, out_x2 - out_x1,
+				  &VAR_scanlineClipperLineScanner)) {
+					goto Continue1;
+				}
+
 				const char *cur_scanline = data;
 				unsigned int cur_byte = *(cur_scanline++);
 				int cur_bit = in_x1 & 7;
 				cur_byte >>= cur_bit;
 
-				for (int x = out_x1; x < out_x2; ++x) {
-					if (cur_bit == 8) {
-						cur_bit = 0;
-						cur_byte = *(cur_scanline++);
+				uint16_t *pixels2 = pixels;
+				do {
+					pixels2 += spanScanner.skip;
+					// FIXME: optimize
+					for (int i = 0; i < spanScanner.skip; ++i) {
+						if (cur_bit == 8) {
+							cur_bit = 0;
+							cur_byte = *(cur_scanline++);
+						}
+						++cur_bit; cur_byte >>= 1;
 					}
-					if (cur_byte & 1) {
-						*pixels = pixel;
+					for (int i = 0; i < spanScanner.width; ++i) {
+						if (cur_bit == 8) {
+							cur_bit = 0;
+							cur_byte = *(cur_scanline++);
+						}
+						*pixels2 = (cur_byte & 1) ? pixel2 : pixel1;
+						++cur_bit; cur_byte >>= 1; ++pixels2;
 					}
-					++cur_bit; cur_byte >>= 1; ++pixels;
-				}
+				} while (TWScanlineClipperLineAdvance(&spanScanner));
+
+			Continue1:
+				pixels += tgt_w;
 				data += stride;
-				pixels += tgt_w - (out_x2 - out_x1);
-			}
+			} while (TWScanlineClipperMoveToNextLine(&VAR_scanlineClipper, &VAR_scanlineClipperLineScanner));
 			break;
 		}
 		case TWPixelFormat32bppRGBX: {
-			uint32_t *in_pixels = (uint32_t *)data + in_x1;
-			for (int y = out_y1; y < out_y2; ++y) {
-				for (int x = out_x1; x < out_x2; ++x) {
-					*(pixels++) = RGBX32ToRGB565(*(in_pixels++));
+			const uint32_t *in_pixels = (const uint32_t *)data + in_x1;
+
+			do {
+				pixels += tgt_w * VAR_scanlineClipperLineScanner.skip;
+				in_pixels += src_w * VAR_scanlineClipperLineScanner.skip;
+
+				if (!TWScanlineClipperStartLine(&VAR_scanlineClipper, &spanScanner, out_x1, out_x2 - out_x1,
+				  &VAR_scanlineClipperLineScanner)) {
+					goto Continue2;
 				}
-				pixels += tgt_w - (out_x2 - out_x1);
-				in_pixels += src_w - (out_x2 - out_x1);
-				// note: out_x2 - out_x1 == in_x2 - in_x1
-				//       using common subexpressions allows optimization
-			}
+
+				const uint32_t *in_pixels2 = in_pixels;
+
+				uint16_t *pixels2 = pixels;
+				do {
+					pixels2 += spanScanner.skip;
+					in_pixels2 += spanScanner.skip;
+					for (int i = 0; i < spanScanner.width; ++i) {
+						*(pixels2++) = RGBX32ToRGB565(*(in_pixels2++));
+					}
+				} while (TWScanlineClipperLineAdvance(&spanScanner));
+
+			Continue2:
+				pixels += tgt_w;
+				in_pixels += src_w;
+			} while (TWScanlineClipperMoveToNextLine(&VAR_scanlineClipper, &VAR_scanlineClipperLineScanner));
+
 			break;
 		}
 		case TWPixelFormat16bppRGB565: {
-			uint16_t *in_pixels = (uint16_t *)data + in_x1;
-			for (int y = out_y1; y < out_y2; ++y) {
-				for (int x = out_x1; x < out_x2; ++x) {
-					*(pixels++) = *(in_pixels++);
+			const uint16_t *in_pixels = (const uint16_t *)data + in_x1;
+
+			do {
+				pixels += tgt_w * VAR_scanlineClipperLineScanner.skip;
+				in_pixels += src_w * VAR_scanlineClipperLineScanner.skip;
+
+				if (!TWScanlineClipperStartLine(&VAR_scanlineClipper, &spanScanner, out_x1, out_x2 - out_x1,
+				  &VAR_scanlineClipperLineScanner)) {
+					goto Continue3;
 				}
-				pixels += tgt_w - (out_x2 - out_x1);
-				in_pixels += src_w - (out_x2 - out_x1);
-				// note: out_x2 - out_x1 == in_x2 - in_x1
-				//       using common subexpressions allows optimization
-			}
+
+				const uint16_t *in_pixels2 = in_pixels;
+
+				uint16_t *pixels2 = pixels;
+				do {
+					pixels2 += spanScanner.skip;
+					in_pixels2 += spanScanner.skip;
+					for (int i = 0; i < spanScanner.width; ++i) {
+						*(pixels2++) = *(in_pixels2++);
+					}
+				} while (TWScanlineClipperLineAdvance(&spanScanner));
+
+			Continue3:
+				pixels += tgt_w;
+				in_pixels += src_w;
+			} while (TWScanlineClipperMoveToNextLine(&VAR_scanlineClipper, &VAR_scanlineClipperLineScanner));
+
 			break;
 		}
 		default:
@@ -255,6 +374,7 @@ eGraphicsDeviceOutput_drawBitmap(CELLIDX idx, const char* data, TWPixelFormat fo
 			assert(0);
 			break;
 	}
+
 }
 
 /* #[<ENTRY_FUNC>]# eGraphicsDeviceOutput_update
